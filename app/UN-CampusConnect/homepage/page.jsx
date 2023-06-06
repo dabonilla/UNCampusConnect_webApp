e3e4bf0a767ad8c909e107e556817d9ff4013c2a
Julian Sanchez <juliandavidsanchezsanabria@gmail.com>
2023-06-05 22:20:13 -0500

    Cambios de homepage y consumo

--- app/UN-CampusConnect/homepage/page.jsx
+++ app/UN-CampusConnect/homepage/page.jsx
@@ -1,9 +1,43 @@
+'use client'
+import Link from 'next/link';
+import axios from "axios";
+import React, { useState, useEffect } from 'react';
 import { Navigation } from './../../components/Navigation';
 import Image from 'next/image';
 import styles from './homepage.module.css';
 
+const endpoint = `http://${process.env.API_GATEWAY_URL}:${process.env.API_GATEWAY_PORT}/graphql`
 
 export default function HomeStudent(){
+
+    const [publicaciones, setPublicaciones] = useState([]);
+  
+    useEffect(() => {
+        const queryexterno = `
+        query 
+        {
+            getExternalUsers {
+                numUsers
+            }
+        }
+      `;
+    
+        axios.post(endpoint, { query: queryexterno })
+          .then(response => {
+            if (response.data.hasOwnProperty('errors')) {
+              console.log("USUARIO NO ENCONTRADO O CONTRASEÑA INVALIDA !!!");
+            } else {
+              console.log("Datos recibidos:");
+              console.log("Test1:", response.data.data.getExternalUsers.numUsers);
+              setPublicaciones(response.data.data.getExternalUsers.numUsers);
+            }
+          })
+          .catch(error => {
+            console.log("Error:", error);
+            // Aquí puedes manejar el error de acuerdo a tus necesidades
+          });
+      }, []); // Se ejecuta solo una vez al montar el componente
+    
     return(
         <div>
             <div className='fixed-top'>
@@ -54,6 +88,30 @@ export default function HomeStudent(){
                             </div>
                         </div>
                     </div>
+                </div>
+            </div>
+            <div className="container mt-4  ">
+                <div className={` ${styles.padre} row`}>
+                    <div className={` ${styles.titulo1} col-6 d-flex flex-column`}>
+                        <h1 className="mb-4 mt-4">Si no encuentras lo que buscabas</h1>
+                        <p> Te recomendamos visitar TutoAcademy. TutoAcademy es una plataforma en línea que ofrece una amplia variedad de cursos y tutoriales en diversos temas. Con muchos de usuarios satisfechos, TutoAcademy puede ser una excelente opción para expandir tus conocimientos. ¡Visita su página web para descubrir todo lo que tienen para ofrecer!</p>
+                            <div className={`d-flex flex-row row ${styles.titulointro} `}>
+                                <div className='col-6 d-flex align-items-center justify-content-center'> 
+                                    <h5 className="mb-4 mt-3 text-center">Numero de Usuarios:</h5>
+                                </div>
+                                <div className='col-6 d-flex align-items-center justify-content-center'>
+                                    <h5> {publicaciones}</h5>
+                                </div>
+                            </div>
+                    </div>
+                    <div className='col-6 d-flex align-items-center justify-content-center'>
+                        <Image
+                            src="/NoBackgroundLogo.png"
+                            width={450}
+                            height={450}
+                            alt="Logo UnCampusConnect"
+                            />
+                    </div>  
                 </div>
             </div>
         </div>
